b045548d2cf9c6c445e79307690d53f07223c043
luisricardo494 <luisricardo494@gmail.com>
2021-09-08 00:00:12 -0400

    first commit

---
+++ src/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      test test
+    </div>
+  );
+}
+
+export default App;
