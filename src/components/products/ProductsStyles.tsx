049e578f3317ad8aaaff57003f323edfa6d8164a
luisricardo494 <luisricardo494@gmail.com>
2021-09-14 21:36:20 -0400

    Added fake REST api and grid layout for products

--- src/components/products/ProductsStyles.tsx
+++ src/components/products/ProductsStyles.tsx
@@ -1,11 +1,44 @@
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
-    wrapper: {
-        minHeight: '50rem',
+    root: {
+        // position: 'relative',
+        // minHeight: '50rem',
         // padding: '7rem 5rem 4rem 5rem',
+        padding: '5rem 2rem',
         backgroundColor: '#438bce',
-        color: 'white'
+        color: 'white',
+    },
+    wrapper : {
+        margin: 'auto',
+        maxWidth: '120rem',
+        // position: 'relative',
+        // height: '100%',
+        // display: 'flex',
+        gap: '3rem',
+        display: 'grid',
+        justifyItems: 'center',
+        // justifyContent: 'space-evenly',
+        // alignContent: 'center',
+        // alignItems: 'center',
+        gridTemplateColumns: 'repeat(4, 1fr)',
+
+        [theme.breakpoints.down("md")]: {
+            display: 'grid',
+            justifyItems: 'center',
+            gridTemplateColumns: 'repeat(3, 1fr)',
+
+        },
+        [theme.breakpoints.down("sm")]: {
+   
+            gridTemplateColumns: 'repeat(2, 1fr)',
+
+        },
+        [theme.breakpoints.down("xs")]: {
+
+            gridTemplateColumns: 'repeat(1, 1fr)',
+
+        },
     }
 
 
--- src/components/products/product/Product.tsx
+++ src/components/products/product/Product.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { FC, useEffect, useState } from 'react'
+import axios from 'axios'
+import useStyles from './ProductStyles';
 
-const Product = () => {
+
+import { Card, CardContent, CardHeader, CardMedia } from '@material-ui/core'
+import { productTypes } from '../Products'
+
+
+
+
+
+const Product:FC<productTypes> = ({productAvocado}) => {
+    const classes = useStyles();
+    // useEffect(() => {
+    //     const fetchProducts = async () => {
+    //         // const res = await fetch(
+    //         //     `http://localhost:3000/products`,
+    //         // )
+    //         // const data = await res.json()
+    //         // console.log(data)
+
+
+    //         const {data} = await axios({
+    //             method: 'get',
+    //             url: `http://localhost:3000/products`,
+    //         })
+       
+    //         console.log(data)
+    //     }
+    //     fetchProducts()
+  
+    // }, [])
+
+
+
+
+
     return (
         <div>
-            
+            <Card className={classes.root}>
+                    {/* {productAvocado.name} */}
+    
+                {/* <CardMedia/> */}
+                <CardHeader title={productAvocado.name}/>
+                <CardContent>
+                    {productAvocado.info}
+                </CardContent>
+            </Card>
         </div>
     )
 }
